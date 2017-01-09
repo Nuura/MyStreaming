d678d44fe0fcdb27e07a6739662457ec57867cf1
Nuura <Nuura@users.noreply.github.com>
2017-01-09 13:44:48 +0100

    Beginnning categories PHP

--- index.php
+++ index.php
@@ -5,4 +5,11 @@ if(isset($_SESSION['pseudo']))
   echo "Bonjour ".$_SESSION['pseudo'];
 else
   echo "Vous n'êtes pas connecté ! <a href=register.php>S'inscrire ?</a> ";
+if(isset($_SESSION))
+  {
+    echo "<input type=text></input>";
+  }
+else
+  {
+  }
 ?>
