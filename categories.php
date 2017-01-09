8bca86aea4a7f00543f049bc8e5cdbae0c606a26
Nuura <Nuura@users.noreply.github.com>
2017-01-09 14:43:51 +0100

    Begin Categories.php

--- categories.php
+++ categories.php
@@ -1,3 +1,10 @@
 <?php
+$bdd = new PDO('mysql:host=localhost;dbname=Streaming;charset=utf8', 'root', 'root');
+$result = $bdd->query("SELECT Libelle FROM Categories;");
+$donnees = $bdd->fetch($result);
 
+for($i = 0; $donnees > $i; $i++)
+  {
+    echo $donnees[$i];
+  }
 ?>
