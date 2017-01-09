b5ad301bf878933076966af161fa91dc8ecb06c0
Nuura <Nuura@users.noreply.github.com>
2017-01-09 21:48:51 +0100

    yolo

--- chosencategories.php
+++ chosencategories.php
@@ -6,7 +6,7 @@ $result = $bdd->query('SELECT Titre, Synopsis, Realisateur, Movie_ID, RelYear FR
 $donnees = $result->fetchAll();
 for($i = 0; count($donnees[$i]) > $i; $i++)
   {
-    echo "<div class=presentation><div class=contenu><img src=img/".$_GET['id'].".jpg class=affiche><div class=ecrit>";
+    echo "<div class=presentation><div class=contenu><img height=450 src=img/".$_GET['id'].".jpg class=affiche><div class=ecrit>";
     echo "<h2>".$donnees[$i]['Titre']."</h2><br>";
     echo "<h5 class=fade>Date de Sortie :</h5><h5 class=date>".$donnees[$i]['RelYear']."</h5><br>";
     echo "<h5 class=fade>Réalisateur : ".$donnees[$i]['Realisateur']."</h5><br>";
