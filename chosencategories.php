812f4422b4283e4ed300827aa50da6350ba60053
Nuura <Nuura@users.noreply.github.com>
2017-01-09 16:46:35 +0100

    CSS Page du film

--- chosencategories.php
+++ chosencategories.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-//require_once('chosencategories.html');
+require_once('chosencategories.html');
 $bdd = new PDO('mysql:host=localhost;dbname=Streaming;charset=utf8', 'root', 'root');
 $result = $bdd->query('SELECT Titre, Synopsis, Realisateur, Movie_ID, RelYear FROM Films JOIN Categories ON Categories.categorie_ID = Films.Movie_ID WHERE Categories.categorie_ID = '.$_GET['id'].';');
 $donnees = $result->fetchAll();
